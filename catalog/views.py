ec05c7e0ce3ce4c70647cef5e6e89acc85150599
Kerbin Original <aakolomeycev@gmail.com>
2026-06-15 11:18:49 +0300

    created html, css and other app init

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def contacts(request):
+    if request.method == 'POST':
+        print(f'Данные успешно отправлены методом POST.\n'
+              f'Содержимое:\n'
+              f'{[i for i in request.POST.items() if i[0] != 'csrfmiddlewaretoken']}')
+    return render(request, 'contacts.html')
