3740980fab522a7cf185f75197638b04393f1df2
Kerbin Original <aakolomeycev@gmail.com>
2026-06-22 17:19:20 +0300

    homework 23

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.CharField(max_length=300, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.pk}. {self.name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.CharField(max_length=300, verbose_name='Описвание')
+    image = models.CharField(max_length=150, verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    created_at = models.DateField()
+    updated_at = models.DateField()
+
+    def __str__(self):
+        return f'{self.pk}. {self.name}. {self.price}. {self.category}'
