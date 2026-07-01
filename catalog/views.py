be0028be7bf10bc5d9b8d60c94d02c3eb69eba82
Kerbin Original <aakolomeycev@gmail.com>
2026-07-01 13:31:24 +0300

    small fix

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 
-from DjangoTask.settings import MEDIA_URL, MEDIA_ROOT
+from DjangoTask.settings import MEDIA_URL
 from .models import Product
-
 
 def home(request):
     products = Product.objects.values('id', 'name', 'description', 'image', 'category__name', 'price')
     for i in products:
         i['image'] = MEDIA_URL + i['image']
-        i['description'] = i['description'][:97] + '...'
     data = {
         'products': products,
     }
@@ -26,6 +24,8 @@ def contacts(request):
 def product(request, product_id):
     product_info = Product.objects.values('name', 'description', 'image', 'category__name', 'price').filter(
         pk=product_id).first()
+    if not product_info:
+        return render(request, 'product_not_found.html')
     product_info['image'] = MEDIA_URL + product_info['image']
     print(product_info)
     data = {
